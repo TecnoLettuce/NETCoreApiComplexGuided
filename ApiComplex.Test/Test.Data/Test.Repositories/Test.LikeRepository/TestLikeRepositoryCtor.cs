85ff0e5b47cb9f5d82b0a6a870e06b3f1c2b1b18
Gabrus <gabriel.sanchez@softwareone.com>
2021-11-02 17:03:05 +0100

    Creado el test del ctor del repositorio like

---
+++ ApiComplex.Test/Test.Data/Test.Repositories/Test.LikeRepository/TestLikeRepositoryCtor.cs
@@ -0,0 +1,26 @@
+﻿using APIComplexEntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using APIComplexEntityFramework.Data.Repositories;
+using Xunit;
+
+namespace ApiComplex.Test.Test.Data.Test.Repositories.Test.LikeRepository
+{
+    public class TestLikeRepositoryCtor
+    {
+        private readonly ApiContext _context;
+
+        [Fact]
+        public void TestRepositoryCtor()
+        {
+            APIComplexEntityFramework.Data.Repositories.LikeRepository likeRepository = new APIComplexEntityFramework.Data.Repositories.LikeRepository(_context);
+
+            Assert.NotNull(likeRepository);
+            Assert.IsType<APIComplexEntityFramework.Data.Repositories.LikeRepository>(likeRepository);
+        }
+
+    }
+}
---
+++ ApiComplex.Test/Test.Data/Test.Repositories/Test.LikeRepository/TestLikeRepositoryGetLikeByUserIdAsync.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiComplex.Test.Test.Data.Test.Repositories.Test.LikeRepository
+{
+    class TestLikeRepositoryGetLikeByUserIdAsync
+    {
+    }
+}
